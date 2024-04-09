2ee4c04c7b2266932464eff6002e59dfef11e3d2
Lucca Menegatti <luccamenegatti@overdriveconsultoria.com.br>
2024-04-09 16:56:56 -0300

    Resolvendo rotas, e redirecionando a home

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::redirect('/', '/users');
+
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
 Route::post('/users', [UserController::class, 'store'])->name('users.store');
